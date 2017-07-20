f86bf8ecefad89b65fde7f1d85eacce44cae4204
Ulrich Schmidt <admin@deruli.de>
2017-07-20 16:53:10 +0200

    refactor

--- src/content/modules/simple_md5_generator/lang/en.php
+++ src/content/modules/simple_md5_generator/lang/en.php
@@ -1,6 +1,5 @@
 <?php
-define ( "TRANSLATION_SIMPLE_MD5_GENERATOR", "Simple MD5 Generator" );
-define ( "TRANSLATION_ENTER_TEXT", "Enter Text" );
-define ( "TRANSLATION_GENERATE", "Generate" );
-define ( "TRANSLATION_RESULT", "Result:" );
-?>
+define("TRANSLATION_SIMPLE_MD5_GENERATOR", "Simple MD5 Generator");
+define("TRANSLATION_ENTER_TEXT", "Enter Text");
+define("TRANSLATION_GENERATE", "Generate");
+define("TRANSLATION_RESULT", "Result:");
